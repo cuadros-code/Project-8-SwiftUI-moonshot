9eb79c29628cccfa59f99ff35c91e193d59d530d
kevin cuadros <kevindavidcuadros@gmail.com>
2024-11-08 16:24:26 -0500

    Initial Commit

---
+++ Project-8-SwiftUI-moonshot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project-8-SwiftUI-moonshot
+//
+//  Created by Kevin Cuadros on 8/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project-8-SwiftUI-moonshot/Project_8_SwiftUI_moonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_8_SwiftUI_moonshotApp.swift
+//  Project-8-SwiftUI-moonshot
+//
+//  Created by Kevin Cuadros on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Project_8_SwiftUI_moonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
