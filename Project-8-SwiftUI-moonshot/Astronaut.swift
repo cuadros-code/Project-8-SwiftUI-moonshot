343bc5e2c46327c2613ef7b53185aa70112a2f13
kevin cuadros <kevindavidcuadros@gmail.com>
2024-11-14 20:50:21 -0500

    add assets

---
+++ Project-8-SwiftUI-moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Project-8-SwiftUI-moonshot
+//
+//  Created by Kevin Cuadros on 14/11/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Project-8-SwiftUI-moonshot/Bundle-Decodable.swift
@@ -0,0 +1,8 @@
+//
+//  Bundle-Decodable.swift
+//  Project-8-SwiftUI-moonshot
+//
+//  Created by Kevin Cuadros on 14/11/24.
+//
+
+import Foundation
